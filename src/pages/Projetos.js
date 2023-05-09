6ae23bc7ae5de50122cf0435fe7e2576adee7e97
PauloVitorMartins <pm885144@gmail.com>
2023-05-09 14:33:35 -0300

    adicionadaLogicaRenderizaçãoMobile

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,5 +1,5 @@
 import Header from '../components/Header';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import SlideImage from '../components/slideImage';
 import { SwiperSlide, Swiper } from 'swiper/react';
@@ -23,6 +23,63 @@ import paletaDeCores from '../images/paletaDeCores.png';
 import toDo from '../images/toDo.png';
 
 export default function Projetos () {
+  let isMobile = useRef(false);
+  const [mobile, setMobile] = useState(false);
+  useEffect(() => {
+    const resize = () => {
+      if (window.innerWidth > 600) {
+        if(isMobile.current) {
+          isMobile.current = false;
+          setMobile(false);
+        }
+      } else {
+        if(!isMobile.current) {
+          isMobile.current = true;
+          setMobile(true);
+        }
+      }
+    }
+
+    window.addEventListener('resize', resize);
+    return () => window.removeEventListener('resize', resize);
+  }, []);
+
+  if (!isMobile.current) {
+    return (
+      <div className='everything'>
+        <Header />
+        <div className='main-content'>
+          <p className='chamada'>fique com alguns dos meus projetos!</p>
+          <p className='paragrafos'>Aqui se encontram projetos de front-end, realizados durante meus estudos na Trybe!</p>
+          <p className='paragrafos'>experimente deixar o mouse sobre a imagem para ter uma prévia!</p>
+        </div>
+      <div className='slides-content'>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        slidesPerView={2}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>
+        <SlideImage text={ textNetflix } title='NetflixClone' image={netflix} imageTwo={netflixGif}/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SlideImage text={textRPG} title='Rpg Fantasy' image={rpgFantasy} imageTwo={rpgGif}/>
+        </SwiperSlide>
+        <SwiperSlide><SlideImage text={textPaletaDeCores} title='Paleta de Cores' image={paletaDeCores} /></SwiperSlide>
+        <SwiperSlide><SlideImage text={textTrybewarts} title='Trybewarts' image={trybewarts} /></SwiperSlide>
+        <SwiperSlide><SlideImage text={textGuessColor} title='Jogo advinhe a cor RGB' image={guessTheColor} /></SwiperSlide>
+        <SwiperSlide><SlideImage text={textToDo} title='To Do List' image={toDo} /></SwiperSlide>
+        ...
+      </Swiper>
+      </div>
+      </div>
+    )
+  }
   return (
     <div className='everything'>
       <Header />
@@ -35,7 +92,7 @@ export default function Projetos () {
     <Swiper
       // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
-      slidesPerView={2}
+      slidesPerView={1}
       navigation
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
