278954fe5b3f2df088f638aff9b93fbeb8fd4071
PauloVitorMartins <pm885144@gmail.com>
2023-05-15 09:39:26 -0300

    fixingLinkEmail

--- src/components/Header.js
+++ src/components/Header.js
@@ -50,7 +50,7 @@ export default function Home () {
         <div className='buttons-nav'>
           <a rel="noreferrer" target='_blank' href='https://www.linkedin.com/in/paulo-vitor-martins-silva-dev/' className='navBar-button'><img alt='button to linkedin' className='navImage' src={ linkedinIcon }/></a>
           <a rel="noreferrer" target='_blank' href='https://github.com/PauloVitorMartins' className='navBar-button'><img alt='button to gitHub' className='navImage' src={ gitHubIcon }/></a>
-          <a rel="noreferrer" target='_blank' href='maito:pm885144@gmail.com' className='navBar-button'><img alt='button-to-email' className='navImage' src={ emailIcon }/></a>
+          <a rel="noreferrer" target='_blank' href='mailto:pm885144@gmail.com' className='navBar-button'><img alt='button-to-email' className='navImage' src={ emailIcon }/></a>
           <a rel="noreferrer" target='_blank' href='https://wa.me/5531983114110' className='navBar-button'><img alt='button whatsApp chat' className='navImage' src={ zapIcon }/></a>
         </div>
       </div>
@@ -73,7 +73,7 @@ export default function Home () {
           <p onClick={showSideBar}><img alt='sideBarIcon' className='sideBarIcon' src={sideBarIcon}/></p>
           <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/paulo-vitor-martins-silva-dev/' className='sideBar-button'><img alt='linkedin button' className='sideImage' src={ linkedinIcon }/></a>
           <a target='_blank' rel="noreferrer" href='https://github.com/PauloVitorMartins' className='sideBar-button'><img alt='button to gitHub' className='sideImage' src={ gitHubIcon }/></a>
-          <a target='_blank' rel="noreferrer" href='maito:pm885144@gmail.com' className='sideBar-button'><img alt='button to email' className='sideImage' src={ emailIcon }/></a>
+          <a target='_blank' rel="noreferrer" href='mailto:pm885144@gmail.com' className='sideBar-button'><img alt='button to email' className='sideImage' src={ emailIcon }/></a>
           <a target='_blank' rel="noreferrer" href='https://wa.me/5531983114110' className='sideBar-button'><img alt='button to whatsApp' className='sideImage' src={ zapIcon }/></a>
           </div>
           <p className='navBar-link-a'><Link className='navBar-link' to='/'>HOME</Link></p>
